f241297de59e0263f133b805e9b26d59a6d16c72
Anthony <anthonylee20250@gmail.com>
2026-03-11 15:09:28 +0800

    lil changes

---
+++ app/lecturer/alerts/dashboard/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <div className="bg-white flex-1 ml-0 lg:ml-64 p-4">
+        <h1 className="text-2xl font-bold text-gray-700">PENIS </h1>
+        <h1 className="text-2xl font-bold text-gray-700">Lecturer Overview </h1>
+      </div>
+    </main>
+  )
+}
+
+      
