cc3b68ab3500d270f70a6a93ce66c79f3a176b33
Anthony <anthonylee20250@gmail.com>
2026-03-13 11:11:00 +0800

    sidebar fix for sutdent

--- app/student/layout.tsx
+++ app/student/layout.tsx
@@ -1,18 +1,38 @@
+"use client"
+
+import { useState } from "react"
 import Sidebar from "../components/sidebar"
+import TopNavbar from "../components/navbar"
 import { studentMenu } from "../config/studentMenu"
 
-export default function StudentLayout({ children }) {
+export default function StudentLayout({ children }: { children: React.ReactNode }) {
+  const [sidebarOpen, setSidebarOpen] = useState(false)
 
-return (
-    <div className="flex">
+  const toggleSidebar = () => setSidebarOpen(!sidebarOpen)
+  const closeSidebar = () => setSidebarOpen(false)
+
+  return (
+    <div>
+      {/* Sidebar */}
       <Sidebar
         panelTitle={studentMenu.panelTitle}
         menu={studentMenu.sections}
+        isOpen={sidebarOpen}
+        onClose={closeSidebar}
       />
 
-      <main className="flex-1 p-6">
-        {children}
-      </main>
+      {/* Right Side */}
+      <div className="lg:ml-64 flex flex-col min-h-screen bg-[#F8F8F8]">
+        
+        {/* Top Navbar */}
+        <TopNavbar onMenuClick={toggleSidebar} />
+
+        {/* Page Content */}
+        <main className="p-6 flex-1">
+          {children}
+        </main>
+
+      </div>
     </div>
   )
 }
